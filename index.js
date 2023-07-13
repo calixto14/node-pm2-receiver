6705aa494f34a5ad98879d9530bd1501566e4000
calixto14 <calixto@live.com>
2023-07-13 20:33:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const amqp = require("amqplib");
+var channel, connection;
+const app = express();
+const PORT = process.env.PORT || 4002;
+app.use(express.json());
+app.listen(PORT, () => console.log("Server running at port " + PORT));
+
+
+connectQueue()  // call the connect function
+ 
+async function connectQueue() {
+    try {
+        connection = await amqp.connect("amqp://localhost:5672");
+        channel    = await connection.createChannel()
+        
+        await channel.assertQueue("test-queue")
+        
+        channel.consume("test-queue", data => {
+            console.log(`${Buffer.from(data.content)}`);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
