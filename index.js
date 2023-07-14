dd2ebfd70fa74c126846d9ca8dde4dad8b45a036
calixto14 <calixto@live.com>
2023-07-13 21:07:13 -0300

    testando filas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const amqp = require("amqplib");
+const configs = require("./configs.json")
 var channel, connection;
 const app = express();
 const PORT = process.env.PORT || 4002;
@@ -13,14 +14,31 @@ async function connectQueue() {
     try {
         connection = await amqp.connect("amqp://localhost:5672");
         channel    = await connection.createChannel()
+
+        configs.queues.forEach(async(config)=> {            
+            await channel.assertQueue(config.name)        
+            channel.consume(config.name, async(data) => {               
+                let dados = JSON.parse(Buffer.from(data.content))               
+                await handlers[config.name](dados);
+                channel.ack(data);
+            })
+        });
         
-        await channel.assertQueue("test-queue")
         
-        channel.consume("test-queue", data => {
-            console.log(`${Buffer.from(data.content)}`);
-            channel.ack(data);
-        })
     } catch (error) {
         console.log(error);
     }
+}
+
+const test1=(data)=>{   
+    console.log("test1 " + JSON.stringify(data))
+}
+
+const test2=(data)=>{
+    console.log("test2" + JSON.stringify(data))
+}
+
+const handlers = {
+    "test-queue" : test1,
+    "test-queue2" : test2
 }
